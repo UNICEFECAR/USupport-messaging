df0aed4819ae1010f5d2be2e16d8db3bf27c0b42
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-06-16 17:28:54 +0300

    Add: missing translations

--- service/translations/kk.js
+++ service/translations/kk.js
@@ -1,3 +1,4 @@
 export default {
   chat_not_found_error: "Чат табылмады",
+  user_not_authorized_error: "Клиент тіркелмеген",
 };
--- service/translations/ru.js
+++ service/translations/ru.js
@@ -1,3 +1,4 @@
 export default {
   chat_not_found_error: "Чат не найден",
+  user_not_authorized_error: "Клиент не авторизован",
 };
