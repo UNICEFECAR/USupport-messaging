c128e9fba30ff9ab4167e3db7c9ca0afe087f382
7Digit-main <it-support@7digit.io>
2025-09-11 14:55:35 +0300

    Refactor: uk translations

--- service/translations/uk.js
+++ service/translations/uk.js
@@ -1,4 +1,4 @@
 export default {
   chat_not_found_error: "Чат не знайдено",
-  user_not_authorized_error: "Користувач(ка) не авторизований(а)",
+  user_not_authorized_error: "Користувач/ка неавторизований/а",
 };
